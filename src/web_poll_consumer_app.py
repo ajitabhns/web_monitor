decb3857a36d39424bef9222da65a458ab8818c3
ajitabhns <ajitabh.ns@gmail.com>
2021-04-25 22:33:47 +1000

    code cleanup

--- src/web_poll_consumer_app.py
+++ src/web_poll_consumer_app.py
@@ -1,38 +1,64 @@
-from consumer import Consumer
-from db_writer import DBWriter
-from pollWebEvent import PollWebEvent
 import logging
 import random
 import yaml
 import os
+import json
+from kafka import KafkaConsumer
 from web_logger import get_logger
+from db_writer import DBWriter
+from pollWebEvent import PollWebEvent
+
 from kafka.errors import KafkaError
 
 kafka_config = yaml.load(open(os.path.join(os.path.dirname(__file__), 'config', 'kafka_config.yml')), Loader=yaml.Loader)    
 
 logger = get_logger('web_poll_consumer_app')
 
-def run_consumer():
-    """
-    Main function which pulls the web poll metrices from the web_poll topic
-    and pushes the data to the table web_monitor.
-    """
-    hostname = kafka_config['kafka-test']['hostname']
-    port = kafka_config['kafka-test']['port']
-    cert_folder = kafka_config['kafka-test']['cert_folder']
-    pwe = PollWebEvent()
-    consumer = Consumer(hostname=hostname,
-                        port=port,
-                        group_id='demo_droup',
-                        cert_folder=cert_folder)
-    logger.info(f'bootstrap_connected() - {consumer.consumer.bootstrap_connected()}')
-    try:
-        consumer.subscribe(pwe.topic_name)
-        consumer.run()
-    except KafkaError as exc:
-        logger.error(f'Exception during subscribing to topic - {exc}')
-    finally:
-        consumer.close()
+class WebMonitorApp_C():
+    def __init__(self, kafka_config):
+        self.hostname = kafka_config['kafka-test']['hostname']
+        self.port = kafka_config['kafka-test']['port']
+        self.cert_folder = kafka_config['kafka-test']['cert_folder']
+        self.pwe = PollWebEvent()
+        self.db_writer = DBWriter()
+        self.consumer = KafkaConsumer(
+            client_id = "client1",
+            group_id = "demo_group",
+            bootstrap_servers = self.hostname+":"+str(self.port),
+            security_protocol = "SSL",
+            ssl_cafile=os.path.join(self.cert_folder, "ca.pem"),
+            ssl_certfile=os.path.join(self.cert_folder, "service.cert"),
+            ssl_keyfile=os.path.join(self.cert_folder, "service.key"),
+            value_deserializer = lambda v: json.loads(v.decode('utf-8')),
+            key_deserializer = lambda v: json.loads(v.decode('utf-8')),
+            max_poll_records = 10
+        )
 
+    def run(self):
+        """
+        Main function which pulls the web poll metrices from the web_poll topic
+        and pushes the data to the table web_monitor.
+        """
+      
+        try:
+            logger.info(f'bootstrap_connected() - {self.consumer.bootstrap_connected()}')
+            self.consumer.subscribe([self.pwe.topic_name])
+            
+            for message in self.consumer:
+                try:
+                    logger.info(f"{message.partition} - {message.offset}: k={message.key} v={message.value}")
+                    names = {**message.key, **message.value}
+                    q1 = self.db_writer.create_query_string(names)
+                    self.db_writer.execute(q1, vars=names)
+                except:
+                    logger.error(f"Failed to write message - {message.key} to db")
+
+        except KafkaError as exc:
+            logger.error(f'Exception during subscribing to topic - {exc}')
+            raise
+        finally:
+            self.consumer.close()
+       
 if __name__ == '__main__':
-    run_consumer()
+    wm_c = WebMonitorApp_C(kafka_config)
+    wm_c.run()
--- src/web_poll_producer_app.py
+++ src/web_poll_producer_app.py
@@ -1,42 +1,65 @@
-from producer import Producer
-from pollWebEvent import PollWebEvent
 import logging
 import random
 import yaml
 import os
+import json
+from datetime import datetime, date
 from web_logger import get_logger
+from kafka import KafkaProducer
+from pollWebEvent import PollWebEvent
+from kafka.errors import KafkaTimeoutError
 
 logger = get_logger('web_poll_producer_app')
 
-kafka_config = yaml.load(open(os.path.join(os.path.dirname(__file__), 'config', 'kafka_config.yml')), Loader=yaml.Loader)   
+kafka_config = yaml.load(open(os.path.join(os.path.dirname(__file__), 'config', 'kafka_config.yml')), Loader=yaml.Loader)
 
-def run_producer():
-    """
-    Main function which polls a list of web sites and pushes the metrices 
-    to the poll_web topic.
-    """
-    hostname = kafka_config['kafka-test']['hostname']
-    port = kafka_config['kafka-test']['port']
-    cert_folder = kafka_config['kafka-test']['cert_folder']
-    logger.info(f'hostname={hostname}, port={port}, cert_folder={cert_folder}')
-    producer = Producer(hostname, port, cert_folder)
-    logger.info(f'bootstrap_connected() - {producer.producer.bootstrap_connected()}')
-    pwe = PollWebEvent()
+class WebMonitorApp_P():
+    def __init__(self, kafka_config):
+        self.hostname = kafka_config['kafka-test']['hostname']
+        self.port = kafka_config['kafka-test']['port']
+        self.cert_folder = kafka_config['kafka-test']['cert_folder']
+        self.pwe = PollWebEvent()
+        self.producer = KafkaProducer(
+            bootstrap_servers=self.hostname+":"+str(self.port),
+            security_protocol="SSL",
+            ssl_cafile=os.path.join(self.cert_folder, "ca.pem"),
+            ssl_certfile=os.path.join(self.cert_folder, "service.cert"),
+            ssl_keyfile=os.path.join(self.cert_folder, "service.key"),
+            value_serializer=lambda v: json.dumps(v, default=self.json_serial).encode('utf-8'),
+            key_serializer=lambda v: json.dumps(v, default=self.json_serial).encode('utf-8')
+        )
 
-    while True:
-        try:
-            website = random.choice(pwe.websites)
-            key, value = pwe.web_monitor(website.get('url'), regex=website.get('regex'))
-        except:
-            logger.error(f"Request failed for {website.get('url')}")
-            continue
+    def run(self):
+        """
+        Main function which polls a list of web sites and pushes the metrices 
+        to the poll_web topic.
+        """
+        logger.info(f'bootstrap_connected() - {self.producer.bootstrap_connected()}')
 
-        try:
-            logger.info(f"Sending message {pwe.topic_name} - {key} - {value}")
-            producer.send(pwe.topic_name, key, value)
-        except:
-            logger.error(f"Failed to send message {pwe.topic_name} - {key} - {value}")
-            continue
+        while True:
+            try:
+                website = random.choice(self.pwe.websites)
+                key, value = self.pwe.web_monitor(website.get('url'), regex=website.get('regex'))
+            except:
+                logger.error(f"Request failed for {website.get('url')}")
+                continue
+
+            try:
+                logger.info(f"Sending message {self.pwe.topic_name} - {key} - {value}")
+                self.producer.send(self.pwe.topic_name, key, value)
+                self.producer.flush()
+            except KafkaTimeoutError as e:
+                logger.error(f"{e} {self.pwe.topic_name} - {key} - {value}")
+                continue
+
+    @staticmethod
+    def json_serial(obj):
+        """JSON serializer for objects not serializable by default json code"""
+
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        raise TypeError ("Type %s not serializable" % type(obj))
 
 if __name__ == '__main__':
-    run_producer()
+    wm_p = WebMonitorApp_P(kafka_config)
+    wm_p.run()
